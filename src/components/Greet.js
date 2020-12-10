fc7f6e02390b3fc75c1a929aa30eca1ae79bd54b
Rishi Wadekar <>
2020-12-09 22:05:25 -0500

    Stateless functional component

---
+++ src/components/Greet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// normal function
+// function Greet() {
+//   return <h1>Hello Rishi</h1>
+// }
+
+// ES6 function
+const Greet = () => <h1>Hello Rishi</h1>
+
+export default Greet;
