2c7df644774a13d2b1b39b0fe4dc9b985f029aac
Rishi Wadekar <rishiwadekar@com>
2020-12-12 12:51:23 -0500

    Event Handling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
 import React, { Component } from 'react';
-import Hello from './components/Hello';
-import Message from './components/Message'
-import Counter from './components/Counter'
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Welcome name="Thor" hero="Thor Odinson">
+        <ClassClick />
+        <FunctionClick />
+        {/*<Welcome name="Thor" hero="Thor Odinson">
         </Welcome>
-        {/*<Counter />
+        <Counter />
         <Message />
         <Greet name="Tony" hero="Ironman">
           <p>This is children props.</p>
