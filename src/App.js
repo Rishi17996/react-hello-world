f738361a69608ab640d4d1903c89f5de75eb6692
Rishi Wadekar <rishiwadekar@com>
2020-12-12 13:11:54 -0500

    Binding Event Handlers

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import React, { Component } from 'react';
 // import Hello from './components/Hello';
 // import Message from './components/Message';
 // import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ClassClick />
+        <EventBind />
+        {/*<ClassClick />
         <FunctionClick />
-        {/*<Welcome name="Thor" hero="Thor Odinson">
+        <Welcome name="Thor" hero="Thor Odinson">
         </Welcome>
         <Counter />
         <Message />
