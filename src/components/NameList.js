25fec53dcad3589ed1a1d7ef6172e96e8b8f2679
Rishi Wadekar <rishiwadekar@com>
2020-12-12 17:39:52 -0500

    Index as Key Anti-pattern

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -3,35 +3,35 @@ import Person from './Person'
 
 function NameList() {
 
-    // const names = ['Clint', 'Wanda', 'Vision']
-    // const nameList = names.map(name => <h2>{name}</h2>)
-    // return <div>{nameList}</div>
+    const names = ['Clint', 'Wanda', 'Vision', 'Clint']
+    const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+    return <div>{nameList}</div>
 
-    const persons = [
-      {
-        id: 1,
-        name: 'Clint',
-        age: 30,
-        color: 'Red'
-      },
-      {
-        id: 2,
-        name: 'Wanda',
-        age: 25,
-        color: 'Purple'
-      },
-      {
-        id: 3,
-        name: 'Vision',
-        age: 5,
-        color: 'Blue'
-      }
-    ]
-
-    const personList = persons.map(person => (
-      <Person key={person.id} person={person}></Person>
-    ))
-    return <div>{personList}</div>
+    // const persons = [
+    //   {
+    //     id: 1,
+    //     name: 'Clint',
+    //     age: 30,
+    //     color: 'Red'
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Wanda',
+    //     age: 25,
+    //     color: 'Purple'
+    //   },
+    //   {
+    //     id: 3,
+    //     name: 'Vision',
+    //     age: 5,
+    //     color: 'Blue'
+    //   }
+    // ]
+    //
+    // const personList = persons.map(person => (
+    //   <Person key={person.id} person={person}></Person>
+    // ))
+    // return <div>{personList}</div>
 }
 
 export default NameList
