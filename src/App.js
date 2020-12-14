cd34c6e27ffbfa690bde2ea1af67f0062c72b652
Rishi Wadekar <rishiwadekar@com>
2020-12-14 08:35:21 -0500

    Fragments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-// import logo from './logo.svg';
-import './App.css';
-// import Greet from './components/Greet';
-// import Welcome from './components/Welcome';
-import React, { Component } from 'react';
-// import Hello from './components/Hello';
-// import Message from './components/Message';
-// import Counter from './components/Counter';
-// import FunctionClick from './components/FunctionClick';
-// import ClassClick from './components/ClassClick';
-// import EventBind from './components/EventBind';
-// import ParentComponent from './components/ParentComponent';
-// import UserGreeting from './components/UserGreeting';
-// import NameList from './components/NameList';
-// import Form from './components/Form';
-import LifecycleA from './components/LifecycleA'
+// import logo from './logo.svg'
+import './App.css'
+// import Greet from './components/Greet'
+// import Welcome from './components/Welcome'
+import React, { Component } from 'react'
+// import Hello from './components/Hello'
+// import Message from './components/Message'
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick'
+// import ClassClick from './components/ClassClick'
+// import EventBind from './components/EventBind'
+// import ParentComponent from './components/ParentComponent'
+// import UserGreeting from './components/UserGreeting'
+// import NameList from './components/NameList'
+// import Form from './components/Form'
+// import LifecycleA from './components/LifecycleA'
+import FragmentDemo from './components/FragmentDemo'
+import Table from './components/Table'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LifecycleA />
+        {/*<FragmentDemo />*/}
+        <Table />
+        {/*s<LifecycleA />/*}
         {/*<Form />*/}
         {/*<NameList />*/}
         {/*<UserGreeting />*/}
---
+++ src/components/Columns.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Columns() {
+
+    return (
+        <>
+            <td> Name </td>
+            <td> Rishi </td>
+        </>
+    )
+}
+
+export default Columns
